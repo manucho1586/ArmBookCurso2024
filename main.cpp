5bfd511088884537ef66cbab2da8e6c010ed4f44
manucho1586 <ing.emanuelgarcia95@gmail.com>
2024-07-07 19:21:24 +0000

    cambio

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #include "mbed.h"
 #include "Dht11.h"
 #include "arm_book_lib.h"
+#include <cstdint>
 
-static UnbufferedSerial pc(USBTX, USBRX);
+
 
 int main()
 {
     DigitalIn B1(BUTTON1);
     Dht11 sensor(D1);
+    UnbufferedSerial pc(USBTX, USBRX);
     DigitalOut Teyu(LED1);
     char buffer[9];
 
     // Set desired properties (9600-8-N-1).
     pc.baud(9600);
     pc.format(8, SerialBase::None, 1);
+    uint8_t temperatura;
 
     while (true)
     {
         Teyu = !B1;
-        sprintf(buffer, "T: %.2d", sensor.getCelsius());
+        temperatura=sensor.getCelsius();
+        sprintf(buffer, "T: %.2d", temperatura);
         pc.write(buffer,9);
-        delay(2000);
+        delay(1000);
     }
 }
